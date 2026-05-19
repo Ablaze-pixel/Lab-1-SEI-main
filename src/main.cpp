6dde37f1a6d3a03e8895cf2e68ef4a8e8676797b
Ablaze-pixel <scripnic587@gmail.com>
2026-05-19 12:55:52 +0300

    commit

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,13 @@
 #include "app_lab_3_2/app_lab_3_2.h"
 #include "app_lab_5_1/app_lab_5_1.h"
 #include "app_lab_5_2/app_lab_5_2.h"
-
+//aloha
 void setup()
 {
-    app_lab_5_2_setup();
+    app_lab_3_2_setup();
 }
 
 void loop()
 {
-    app_lab_5_2_run();
+    app_lab_3_2_run();
 }
